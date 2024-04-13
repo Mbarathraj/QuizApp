855c57de2347e8f5e6b14b12005acbfe81de51b8
Mbarathraj <barathmurugesan0@gmail.com>
2024-04-14 00:25:25 +0530

    Delete Login.jsx and  index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,91 @@
 import './App.css'
 import QuizComponent from './Components/QuizComponent'
-import Header from './Components/Header'
-import Questions from './Components/Main'
-import Footer from './Components/Footer'
+// import Header from './Components/Header'
+// import Questions from './Components/Main'
+// import Footer from './Components/Footer'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import beepAudio from './Audio/beep-6-96243.mp3';
+
 import Login1 from './Login/Login1'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './Components/Dashboard/Dashboard'
 import { useState } from 'react'
-import Admin from '../Admin/Admin'
-import AdminaddQuestion from '../Admin/AdminaddQuestion'
+
 
 function App() {
   let user = JSON.parse(localStorage.getItem("userInfo"))
-  // console.log(user)
+  
   const [userInfo, setUserInfo] = useState(user)
   return (
     <>
+
       <BrowserRouter>
         <Routes>
+
+          {/* Login */}
+
           <Route path='/login' element={<Login1 setUserInfo={setUserInfo} />}></Route>
+
+          {/* Login/Dashboard */}
+
           <Route path='/login/dashboard' element={<Dashboard userInfo={userInfo} />}></Route>
+
+          {/* Login/Dashboard/Quiz */}
+
           <Route path='/login/dashboard/quiz' element={<QuizComponent />}></Route>
+
+          {/* Login/AdminDashboard */}
+          <Route path='/login/admindashboard'></Route>
+
+
         </Routes>
+        <ToastContainer />
       </BrowserRouter>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       {/* <QuizComponent/> */}
-    {/* <QuizComponent/> */}
+      {/* <QuizComponent/> */}
       {/* <Login/> */}
-      
+
       {/* <Header />
       <Footer noOfQuestions={25} />
       <Questions />
       <ToastContainer /> * */}
-       {/* <Login1/>  */} 
-       {/* <BrowserRouter>
+      {/* <Login1/>  */}
+      {/* <BrowserRouter>
        <Routes>
         <Route path='/login' element={<Login1/>}></Route>
         <Route path='/dashboard' element={<Dashboard/>}></Route>
        </Routes>
        </BrowserRouter> */}
-       <div className="main">
-       <Admin/>
-       <AdminaddQuestion/>
-       </div>
-       
+
+
     </>
   )
 }
